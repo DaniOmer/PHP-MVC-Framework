da0718df33a53b71f1061a776586373cead10d85
Omer Dotchamou <scalens_01@MBPdeSCALENS01.lan>
2023-07-10 02:59:12 +0200

    Fixing bug on Frontcontroller -8

--- www/src/views/blog.php
+++ www/src/views/blog.php
@@ -6,3 +6,20 @@
 
 ?>
 
+<section>
+    <h1><?= $template->getTitle() ?></h1>
+    <div>
+        <h2><?= $template->getFirstSubTitle() ?></h2>
+        <p><?= $template->getFirstParagraph() ?></p>
+    </div>
+    <div>
+        <h2><?= $template->getSecondSubTitle() ?></h2>
+        <p><?= $template->getSecondParagraph() ?></p>
+    </div>
+
+    <div>
+        <h2><?= $template->getThirdSubTitle() ?></h2>
+        <p><?= $template->getThirdParagraph() ?></p>
+    </div>
+    
+</section>
